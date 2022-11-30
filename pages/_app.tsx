7e36721ec327b561e7c0e0ade137ff3f41d52a89
Roman Khymych <roman.khymych@icloud.com>
2022-11-30 23:16:52 +0100

    feat: add basic app shell implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,16 @@
-import { AppProps } from 'next/app'
-import Head from 'next/head'
-import { MantineProvider } from '@mantine/core'
+import { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
 
 export default function App(props: AppProps) {
-  const { Component, pageProps } = props
+  const { Component, pageProps } = props;
 
   return (
-    <>
-      <Head>
-        <title>Heracl</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          colorScheme: 'light',
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
-    </>
-  )
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{ colorScheme: "dark" }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,54 @@
-import Head from 'next/head'
+import { useState } from "react";
+import {
+  AppShell,
+  Header,
+  Footer,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from "@mantine/core";
 
 export default function Home() {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+
   return (
-    <div>
-      <Head>
-        <title>Heracl</title>
-        <meta name="description" content="Heracl app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <AppShell
+      styles={{
+        main: {
+          background:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.white,
+        },
+      }}
+      header={
+        <Header height={{ base: 50 }} p="md">
+          <div
+            style={{ display: "flex", alignItems: "center", height: "100%" }}
+          >
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
 
-      <main>
-        <h1>Welcome to Heracl</h1>
-      </main>
-
-      <footer>
-        <a
-          href="https://www.heracl.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by Heracl
-        </a>
-      </footer>
-    </div>
-  )
+            <Text>Heracl</Text>
+          </div>
+        </Header>
+      }
+      footer={
+        <Footer height={60} p="md">
+          Application footer
+        </Footer>
+      }
+    >
+      <Text>Welcome to Heracl</Text>
+    </AppShell>
+  );
 }
