54bffdaf15758a8701994d5bff65f05d93034ad7
Roman Khymych <roman.khymych@icloud.com>
2023-02-05 11:55:55 +0100

    fix: remove temp issue content

--- pages/cookies.tsx
+++ pages/cookies.tsx
@@ -3,64 +3,14 @@ import { Text } from '@mantine/core';
 export default function Cookies() {
   return (
     <Text>
-      cookies Last updated: August 01, 2021 This cookie policy explains what
+      Cookies Last updated: August 01, 2021 This cookie policy explains what
       cookies are and how we use them. You should read this policy so that you
       understand what type of cookies we use or what information we collect
       using cookies and how that information is used. This Cookie Policy was
       created using the Cookie Policy Generator. Cookies do not typically
       contain any information that personally identifies a user, but personal
       information we store about you may be linked to the information stored in
-      and obtained from cookies. For more information on how we use, store and
-      keep your personal information secure, please see our Privacy Policy. We
-      do not store sensitive personal information such as postal addresses,
-      account passwords, etc. in the cookies we use. Interpretation and
-      Definitions interpretation The words whose initials are capitalized have
-      the meanings defined under the following conditions. The following
-      definitions have the same meaning whether they are singular or plural.
-      definitions For the purposes of this cookie policy: Company (referred to
-      as either "the Company", "we", "us" or "our" in this Cookie Policy) refers
-      to HERACL Roman Khymych, ul. Krynicka 3, 80-393 Gdansk. Cookies are small
-      files that are placed on your computer, mobile or other device by a
-      website and contain, among other things, details about your browsing
-      history on that website. Website refers to Heracl, accessible via
-      https://www.heracl.com/ You are the individual accessing or using the
-      Website, or a business or entity on whose behalf such individual is
-      accessing or using the Website, as the case may be. The use of cookies
-      Type of cookies we use Cookies can be "persistent" or "session" cookies.
-      Persistent cookies remain on your computer or mobile device when you go
-      offline, while session cookies are deleted once you close your web
-      browser. We use both session cookies and persistent cookies for the
-      purposes listed below: Necessary / essential cookies Type: session cookies
-      Managed by: Us Purpose: These cookies are essential to provide you with
-      services available through the website and to enable you to use some of
-      its features. They help authenticate users and prevent fraudulent use of
-      user accounts. Without these cookies, the services you have requested
-      cannot be provided and we only use these cookies to provide you with those
-      services. Functionality Cookies Type: Persistent cookies Managed by: Us
-      Purpose: These cookies allow us to remember choices you make when using
-      the website, such as B. Your login data or your language setting. The
-      purpose of these cookies is to offer you a more personalized experience
-      and to avoid having to re-enter your preferences each time you use the
-      website. Your Choices Regarding Cookies If you wish to avoid the use of
-      cookies on the website, you must first disable the use of cookies in your
-      browser and then delete the cookies stored in your browser in connection
-      with this website. You can use this option at any time to prevent the use
-      of cookies. If you do not accept our cookies, you may experience
-      inconveniences when using the website and some functions may not work
-      properly. If you want to delete cookies or instruct your web browser to
-      delete or refuse cookies, please visit your web browser's help pages. For
-      Chrome web browser, please visit this page from Google:
-      https://support.google.com/accounts/answer/32050 For the Internet Explorer
-      web browser, please visit this page from Microsoft:
-      http://support.microsoft.com/kb/278835 For Firefox web browser, please
-      visit this page from Mozilla:
-      https://support.mozilla.org/en-US/kb/delete-cookies-remove-info-websites-stored
-      For the Safari web browser, please visit this page from Apple:
-      https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac
-      For any other web browser, please visit your web browser's official pages.
-      More information about cookies You can learn more about cookies here: All
-      about cookies from TermsFeed. Contact us If you have any questions about
-      this cookie policy, you can contact us: https://www.heracl.com/contact
+      and obtained from cookies.
     </Text>
   );
 }
